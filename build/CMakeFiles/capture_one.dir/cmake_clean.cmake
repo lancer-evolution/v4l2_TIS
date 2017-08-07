a6d4941eeb57f50106912ed6dcd71604a531a7d0
lancer-evolution <m104086k@mail.kyutech.jp>
2017-08-07 12:52:24 +0900

    ignore5

---
+++ build/CMakeFiles/capture_one.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/capture_one.dir/capture_one.o"
+  "../capture_one.pdb"
+  "../capture_one"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/capture_one.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/listup.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/listup.dir/listup.o"
+  "../listup.pdb"
+  "../listup"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/listup.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
